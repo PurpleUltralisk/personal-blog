38361e733579a626fcab549fd5d12776c69f0d15
PurpleUltralisk <uw.david.jiang@gmail.com>
2021-06-24 23:04:15 -0400

    login page

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,95 @@
 import os
-from flask import Flask, render_template, send_from_directory, json, request 
+from flask import (
+    Flask,
+    render_template,
+    send_from_directory,
+    json,
+    request,
+    flash,
+    redirect,
+    session,
+    url_for,
+    logging,
+)
 from dotenv import load_dotenv
-from . import db 
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from app.db import get_db
+
+# from . import auth
 
 load_dotenv()
 
+# Init App
 app = Flask(__name__)
-app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite') 
+app.config["DATABASE"] = os.path.join(os.getcwd(), "flask.sqlite")
+# Init db
 db.init_app(app)
+# app.register_blueprint(auth.bp)
 
-@app.route('/')
+# Routes
+@app.route("/")
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template("index.html")
 
-@app.route('/health')
+
+@app.route("/health")
 def health():
-    return render_template('health.html', url=os.getenv("URL"))
+    return render_template("health.html")
 
-@app.route('/register')
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return 'Registering'
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        db = get_db()
+        error = None
 
-@app.route('/login')
+        if not username:
+            error = "Username is required."
+        elif not password:
+            error = "Password is required."
+        elif (
+            db.execute("SELECT id FROM user WHERE username = ?", (username,)).fetchone()
+            is not None
+        ):
+            error = f"User {username} is already registered."
+
+        if error is None:
+            db.execute(
+                "INSERT INTO user (username, password) VALUES (?, ?)",
+                (username, generate_password_hash(password)),
+            )
+            db.commit()
+            return f"User {username} created successfully"
+        else:
+            return error, 418
+
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return 'Login'
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        db = get_db()
+        error = None
+        user = db.execute(
+            "SELECT * FROM user WHERE username = ?", (username,)
+        ).fetchone()
+
+        if user is None:
+            error = "Incorrect username."
+        elif not check_password_hash(user["password"], password):
+            error = "Incorrect password."
+
+        if error is None:
+            # session.clear()
+            # session["user_id"] = user["id"]
+            # return redirect(url_for("index"))
+            return "Login Successful", 200
+        else:
+            return error, 418
+    return render_template("login.html")
