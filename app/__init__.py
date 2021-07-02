fbcca982906c377c0d4f2011540add3034116f9c
PurpleUltralisk <uw.david.jiang@gmail.com>
2021-07-02 14:39:16 -0400

    alchemy

--- app/__init__.py
+++ app/__init__.py
@@ -11,21 +11,49 @@ from flask import (
     url_for,
     logging,
 )
-from dotenv import load_dotenv
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db
-from app.db import get_db
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+# from dotenv import load_dotenv
+# from . import db
+# from app.db import get_db
 # from . import auth
 
-load_dotenv()
+# load_dotenv()
 
 # Init App
 app = Flask(__name__)
-app.config["DATABASE"] = os.path.join(os.getcwd(), "flask.sqlite")
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}".format(
+    user=os.getenv("POSTGRES_USER"),
+    passwd=os.getenv("POSTGRES_PASSWORD"),
+    host=os.getenv("POSTGRES_HOST"),
+    port=5432,
+    table=os.getenv("POSTGRES_DB"),
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 # Init db
-db.init_app(app)
+# db.init_app(app)
 # app.register_blueprint(auth.bp)
+
+
+class UserModel(db.Model):
+    __tablename__ = "users"
+
+    username = db.Column(db.String(), primary_key=True)
+    password = db.Column(db.String())
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
 
 # Routes
 @app.route("/")
@@ -43,25 +71,19 @@ def register():
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
-        db = get_db()
         error = None
 
         if not username:
             error = "Username is required."
         elif not password:
             error = "Password is required."
-        elif (
-            db.execute("SELECT id FROM user WHERE username = ?", (username,)).fetchone()
-            is not None
-        ):
+        elif UserModel.query.filter_by(username=username).first() is not None:
             error = f"User {username} is already registered."
 
         if error is None:
-            db.execute(
-                "INSERT INTO user (username, password) VALUES (?, ?)",
-                (username, generate_password_hash(password)),
-            )
-            db.commit()
+            new_user = UserModel(username, generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
             return f"User {username} created successfully"
         else:
             return error, 418
@@ -74,15 +96,12 @@ def login():
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
-        db = get_db()
         error = None
-        user = db.execute(
-            "SELECT * FROM user WHERE username = ?", (username,)
-        ).fetchone()
+        user = UserModel.query.filter_by(username=username).first()
 
         if user is None:
             error = "Incorrect username."
-        elif not check_password_hash(user["password"], password):
+        elif not check_password_hash(user.password, password):
             error = "Incorrect password."
 
         if error is None:
