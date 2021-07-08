589a7e1ec59d2df03a1cf83bd56e4005b1bb62f9
Lint Action <lint-action@samuelmeuli.com>
2021-07-08 16:24:24 +0000

    Fix code style issues with Black

--- app/auth.py
+++ app/auth.py
@@ -13,13 +13,13 @@
 
 # 	if user_id is None:
 # 		g.user=None
-# 	else: 
+# 	else:
 # 		g.user = get_db().execute(
 # 			'SELECT * FROM user WHERE id = ?', (user_id, )
 # 		).fetchone()
 
 # @bp.route('/logout')
-# def logout(): 
+# def logout():
 # 	session.clear()
 # 	return redirect(url_for('index'))
 
@@ -28,6 +28,6 @@
 # 	def wrapped_view(**kwargs):
 # 		if g.user is None:
 # 			return redirect(url_for('auth.login'))
-		
+
 # 		return view(**kwargs)
 # 	return wrapped_view
